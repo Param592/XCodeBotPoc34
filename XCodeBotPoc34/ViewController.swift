5a6ccfe10e78ce86037e980bff2f8a69dc731aa4
Param592 <paramjeet.2.kaur@bt.com>
2019-09-20 15:55:24 +0530

    Add new files to git

---
+++ XCodeBotPoc34/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeBotPoc34
+//
+//  Created by CHDSEZ007826DT on 20/09/19.
+//  Copyright © 2019 CHDSEZ007826DT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
